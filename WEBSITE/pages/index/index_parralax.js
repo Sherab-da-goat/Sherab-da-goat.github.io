5b54a9f53769d7b7481007f5ec8c4e738ac5117b
Your Name <you@example.com>
2024-03-11 22:10:33 +0000

    fuck i forgot abt commit :DDDDDD

---
+++ WEBSITE/pages/index/index_parralax.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("scroll", function() {
+    const scrollPosition = window.scrollY;
+    const zoomFactor = 1 + scrollPosition / 500; // Adjust the zoom factor as needed
+    const backgroundSize = `${zoomFactor * 100}%`;
+    document.body.style.backgroundSize = backgroundSize;
+    document.querySelector("body").style.backgroundSize = backgroundSize;
+  });
