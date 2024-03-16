e5f44aeeceac20d48a858de2836a9ba50069e466
Your Name <you@example.com>
2024-03-16 02:40:41 +0000

    iokokkk

--- WEBSITE/index/index_parralax.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("scroll", function() {
-    const scrollPosition = window.scrollY;
-    const zoomFactor = 1 + scrollPosition / 5000; // Adjust the zoom factor as needed
-    const backgroundSize = `${zoomFactor * 100}%`;
-    document.body.style.backgroundSize = backgroundSize;
-    document.querySelector("body").style.backgroundSize = backgroundSize;
-  });
