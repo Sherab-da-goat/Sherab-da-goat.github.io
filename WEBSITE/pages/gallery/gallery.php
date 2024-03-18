316b19d5c0e108b674f24359054f925807f1e2aa
Your Name <you@example.com>
2024-03-18 17:56:47 +0000

    need to move images around because thety aint random aslo make em bigger

--- WEBSITE/pages/gallery/gallery.php
+++
@@ -1,5 +0,0 @@
-<?php
-$imagesFolder = 'images/';
-$images = array_diff(scandir($imagesFolder), array('..', '.')); // Get all files in the folder
-echo json_encode($images); // Send image file names as JSON
-?>
